69ee8e249070440a0d01e2532a9b8b34f4cd7104
董辉 <2906833963@qq.com>
2017-02-19 23:57:33 +0800

    no message

---
+++ KeepLearn/app/src/main/java/com/study/dh/keeplearn/network/PicInfoDetail.java
@@ -0,0 +1,12 @@
+package com.study.dh.keeplearn.network;
+
+/**
+ * Created by dh on 2017/2/19.
+ */
+
+public class PicInfoDetail  extends PicInfo {
+    @Override
+    public String getCreTime() {
+        return super.getCreTime();
+    }
+}
